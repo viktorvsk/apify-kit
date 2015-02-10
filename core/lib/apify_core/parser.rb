52449cf057b1a4c8727ac4526548ece4923a90cd
Viktor Vsk <me@viktorvsk.com>
2015-02-10 12:08:40 +0200

    Optimize fetch

--- core/lib/apify_core/parser.rb
+++ core/lib/apify_core/parser.rb
@@ -15,7 +15,13 @@ module Apify
       end
 
       def self.fetch(expression, docs)
-        docs.map{ |html| ::Nokogiri::HTML(html).search(expression) rescue nil }.flatten
+        res = []
+        docs.each do |html|
+          r = (::Nokogiri::HTML(html).search(expression).to_s rescue nil)
+          res << r
+        end
+        a = res.compact.map{ |node| ::Nokogiri::HTML(node).at_css('body').children rescue nil }.flatten.compact
+        a        
       end
 
       private
